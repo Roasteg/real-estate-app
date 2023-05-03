3cf6186399df726cb965c47699402a11a44475f4
Roasteg <krostislav468@gmail.com>
2023-05-03 12:02:24 +0300

    Init

---
+++ screens/start/StartScreen.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+export default function StartScreen() {
+  return (
+    <View>
+      <Text>StartScreen</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    
+})
